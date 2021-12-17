a5560bf35663fb5c73b95f5a7982d46ff362db40
TatiAhumadaCode <skateandpunk@gmail.com>
2021-12-16 21:00:16 -0500

    first commit

--- script.js
+++ script.js
@@ -1,79 +1,103 @@
-const author = document.getElementById('author').value
-const title = document.getElementById('title').value
-const pages = document.getElementById('pages').value
-const isBookRead = document.querySelector('input[name=read-book]:checked').value
+const author = document.getElementById('author')
+const title = document.getElementById('title')
+const pages = document.getElementById('pages')
+const read = document.querySelector('input[name=read]')
 const addBtn = document.querySelector('button');
 const tableBook = document.querySelector('.table-book');
+const dataForm = document.querySelector('#dataForm');
+
+//event to add the book row to the table
+
+addBtn.addEventListener('click', addBook);
 
 
+let myLibrary = [];
 
+//constructor
 
 class Book {
-    constructor(author, title, pages, isBookRead) {
-        this.author = author
-        this.title = title
-        this.pages = pages
-        this.isBookRead = isBookRead
-    }
-
-    addBook() {
-        const row = tableBook.insertRow(-1);
-        
-        const cell1 = row.insertCell(0);
-        cell1.innerText = `${this.author}`
-
-        const cell2 = row.insertCell(1);
-        cell2.innerText = `${this.title}`
-
-        const cell3 = row.insertCell(2);
-        cell3.innerText = `${this.pages}`
-
-        const cell4 = row.insertCell(3);
-        cell4.setAttribute('id', 'cellActive');
-        cell4.innerText = `${this.isBookRead}`;
-
-        const cell5 = row.insertCell(4);
-        const changeStatus = document.createElement('button');
-        changeStatus.textContent = 'Change status';
-        
-        changeStatus.addEventListener('click', () => {
-
-            if(this.isBookRead === 'yes') {
-                const checked = document.getElementById('cellActive');
-                checked.innerText = "It's not read yet"
-                return;
-            }
-            if(this.isBookRead === 'no') {
-                const checked = document.getElementById('cellActive');
-                checked.innerText = "it's read";
-                return;
-            }
-        })
-
-        const cell6 = row.insertCell(5);
-        const deleteList = document.createElement('button');
-        deleteList.classList.add('deleteBtn');
-        deleteList.textContent = 'Delete';
-
-        cell5.appendChild(changeStatus)
-        cell6.appendChild(deleteList);
-
-        //events inside rows
-        deleteList.addEventListener('click', () => {
-            row.parentNode.removeChild(row)
-        });
-
+    constructor(author, title, pages, read) {
+        this.author = author;
+        this.title = title;
+        this.pages = pages;
+        this.read = read;
     }
 }
 
-const newBook = new Book(author, title, pages, isBookRead);
 
-//events
+//function to render the book and add to the event
 
-addBtn.addEventListener('click', (event) => {
-    event.preventDefault();
-    newBook.addBook(author, title, pages, isBookRead);
+function addBook(event) {
 
-    form.reset();
+    event.preventDefault()
+    createBook();
+    let newBook = new Book(author, title, pages, read);
+    myLibrary.push(newBook);
+    dataForm.reset();
 
-})
+}
+
+//function to create the book's rows
+
+function createBook() {
+    const row = tableBook.insertRow(-1);
+        
+    const cell1 = row.insertCell(0);
+    cell1.innerText = `${author.value}`
+
+    const cell2 = row.insertCell(1);
+    cell2.innerText = `${title.value}`
+
+    const cell3 = row.insertCell(2);
+    cell3.innerText = `${pages.value}`
+
+    const cell4 = row.insertCell(3);
+    cell4.classList.add('row-four');
+
+    const changeStatus = document.createElement('button');
+    changeStatus.classList.add('toggle-btn')
+    
+    if(read.checked === false) {
+        changeStatus.textContent = "It's not read yet";
+        changeStatus.style.backgroundColor = '#f73c53';
+        changeStatus.style.color = 'white';
+        
+    } else {
+        changeStatus.textContent = "It's already read";
+        changeStatus.style.backgroundColor = '#76f074';
+        changeStatus.style.color = 'black';
+    }
+
+    cell4.appendChild(changeStatus)
+    
+    //event to change status' input
+
+    changeStatus.addEventListener('click', () => {
+        if(changeStatus.innerText === "It's not read yet") {
+            changeStatus.innerText = "It's already read";
+            changeStatus.style.backgroundColor = '#76f074';
+            changeStatus.style.color = 'black';
+            this.read = 'yes';
+        } else {
+            changeStatus.innerText = "It's not read yet";
+            changeStatus.style.backgroundColor = '#f73c53';
+            changeStatus.style.color = 'white';
+            this.read = 'no';
+        }
+    })
+
+    const deleteList = document.createElement('i');
+    deleteList.classList.add('far');
+    deleteList.classList.add('fa-trash-alt');
+
+    //event to delete the rows
+
+    cell4.appendChild(deleteList);
+
+    deleteList.addEventListener('click', () => {
+        row.parentNode.removeChild(row)
+    });
+}
+
+
+
